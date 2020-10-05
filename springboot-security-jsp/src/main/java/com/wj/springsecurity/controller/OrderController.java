09fdf8d08de437b0410ab92127729f0a3da62064
Your Name <you@example.com>
2020-10-05 20:18:26 +0800

    提交

--- springboot-security-jsp/src/main/java/com/wj/springsecurity/controller/OrderController.java
+++ springboot-security-jsp/src/main/java/com/wj/springsecurity/controller/OrderController.java
@@ -1,13 +1,8 @@
 package com.wj.springsecurity.controller;
 
-import com.wj.springsecurity.domain.SysUser;
-import com.wj.springsecurity.util.SecurityUtil;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Collection;
 
 @Controller
 @RequestMapping("/order")
--- springboot-security-jsp/src/main/java/com/wj/springsecurity/controller/ProductController.java
+++ springboot-security-jsp/src/main/java/com/wj/springsecurity/controller/ProductController.java
@@ -1,14 +1,8 @@
 package com.wj.springsecurity.controller;
 
-import com.wj.springsecurity.domain.SysUser;
-import com.wj.springsecurity.util.SecurityUtil;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Collection;
 
 @Controller
 @RequestMapping("/product")
